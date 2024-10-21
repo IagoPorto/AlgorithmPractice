b3b3229a7e95fdc70e85c60e4cac185ac8f29e64
Iago Porto <39347809+IagoPorto@users.noreply.github.com>
2024-10-21 08:09:00 +0200

    Create maxUniqueSplit.java

---
+++ dailyCodeChallenge/maxUniqueSplit.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxUniqueSplit(String s) {
+        return backtrack(s, 0, new HashSet<>());
+    }
+
+    private int backtrack(String s, int start, HashSet<String> seen) {
+        if (start == s.length()) {
+            return seen.size();
+        }
+
+        int maxCount = 0;
+        for (int end = start + 1; end <= s.length(); end++) {
+            String substring = s.substring(start, end);
+            if (!seen.contains(substring)) {
+                seen.add(substring);
+                maxCount = Math.max(maxCount, backtrack(s, end, seen));
+                seen.remove(substring);
+            }
+        }
+        return maxCount;
+    }
+
+}
